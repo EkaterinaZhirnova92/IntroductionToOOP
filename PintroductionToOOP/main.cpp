09ec04c61e5e386d63ce30c10d60f593ab1f9a4c
Ekaterina Zhirnova <Katarinaetya4@gamil.com>
2022-11-06 14:32:27 +0300

    commit two

--- PintroductionToOOP/main.cpp
+++ PintroductionToOOP/main.cpp
@@ -1,15 +1,35 @@
 ﻿#include<iostream>
 using namespace std;
 
-struct Point
+class Point
 {
+
 	double x ;
 	double y ;
+public:
+	double get_x()const
+	{
+		return x;
+	}
+	double get_y()const
+	{
+		return y;
+	}
+	void set_x(double x)
+	{
+		this->x = x;
+	}
+	void set_y(double y)
+	{
+		this->y = y;
+	}
 };
 
+//#define STRUCT_POINT
 void main()
 {
 	setlocale(LC_ALL, "");
+#ifdef STRUCT_POINT
 	cout << "Hello OOP" << endl;
 	int a; //объявление переменной а типа int
 	Point A; //объявление переменной А типа Point
@@ -21,4 +41,10 @@ void main()
 
 	Point* pA = &A;
 	cout << pA->x << "\t" << pA->y << endl;
+#endif
+
+	Point A;
+	A.set_x(2);
+	A.set_y(3);
+	cout << A.get_x() << "\t" << A.get_y() << endl;
 }
